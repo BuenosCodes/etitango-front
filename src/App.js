e155661efb9ad4a983af26091898dd0c3bd80f75
Ana Martínez Saucedo <anmartinez@uade.edu.ar>
2022-08-04 18:30:03 -0300

    chore: i18n-next configuration

--- src/App.js
+++ src/App.js
@@ -2,6 +2,21 @@ import {Route, Routes} from "react-router-dom";
 import routes from "./routes/Routes";
 import withRoot from "./components/withRoot";
 import EtiAppBar from "./components/EtiAppBar";
+import i18n from "i18next";
+import {initReactI18next } from "react-i18next";
+import Backend from 'i18next-http-backend';
+
+i18n
+    .use(initReactI18next)
+    .use(Backend)
+    .init({
+        lng: "es", //TODO this could be set from user info
+        fallbackLng: "es",
+        interpolation: {
+            escapeValue: false
+        },
+        nsSeparator: '.'
+    })
 
 function App() {
     return (
