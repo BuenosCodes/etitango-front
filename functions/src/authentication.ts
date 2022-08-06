c54206a183cf28755d0557ce2d8714bcae56f2b3
thehighestprimenumber <maru.c50@gmail.com>
2022-08-06 18:11:11 -0300

    package-lock unignore

--- functions/src/authentication.ts
+++ functions/src/authentication.ts
@@ -1,5 +1,5 @@
 import * as functions from "firebase-functions";
-import {CallableContext} from "firebase-functions/lib/common/providers/https";
+import { CallableContext } from "firebase-functions/lib/providers/https";
 import {db} from "./index";
 
 export const createUserInDB = functions.https.onCall(async (data: {email: string}, context: CallableContext) => {
