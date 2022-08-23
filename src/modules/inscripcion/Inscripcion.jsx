2a0ad8e286802533a59c21cff4d4a816343f4cba
thehighestprimenumber <maru.c50@gmail.com>
2022-08-23 13:54:49 -0300

    fix: form validation when country is not Argentina
    closes #70
    
    chore: restore state and city fields

--- src/modules/inscripcion/Inscripcion.jsx
+++ src/modules/inscripcion/Inscripcion.jsx
@@ -48,8 +48,14 @@ export default function Inscripcion() {
         food: string().required("Este campo no puede estar vacío"),
         isCeliac: bool().required("Este campo no puede estar vacío"),
         country: string().nullable(true).required("Este campo no puede estar vacío"),
-        province: string().nullable(true).required("Este campo no puede estar vacío"),
-        city: string().nullable(true).required("Este campo no puede estar vacío"),
+        province: string().nullable(true).when("country", {
+            is: "Argentina",
+            then: string().nullable(true).required("Este campo no puede estar vacío")
+        }),
+        city: string().nullable(true).when("country", {
+            is: "Argentina",
+            then: string().nullable(true).required("Este campo no puede estar vacío")
+        }),
         dateArrival: date().required("Este campo no puede estar vacío"),
         dateDeparture: date().required("Este campo no puede estar vacío")
     });
