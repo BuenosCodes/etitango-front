bd95f1345b73c402a35303b720c2e252efb171f0
Maru <marina@mycnote.com>
2023-01-24 13:45:12 -0300

    close #135 add fixNumbering function

---
+++ functions/src/superAdmin.ts
@@ -0,0 +1,27 @@
+import {CallableContext} from "firebase-functions/lib/providers/https";
+import {db} from "./index";
+import {firestore} from "firebase-admin";
+import QueryDocumentSnapshot = firestore.QueryDocumentSnapshot;
+import {validateUserIsSuperAdmin} from "./validators";
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const functions = require("firebase-functions");
+
+
+export const fixNumbering = functions.https.onCall(
+    async (etiEventId: string, context: CallableContext) => {
+      await validateUserIsSuperAdmin(context);
+      const signupsRef = db.collection("signups");
+      const signups = await signupsRef
+          .where("etiEventId", "==", etiEventId)
+          .orderBy("orderNumber", "asc")
+          .get();
+      const updates: { id: string, orderNumber: number }[] = [];
+      signups.docs.forEach((doc: QueryDocumentSnapshot, i: number) => {
+        return updates.push({id: doc.id, orderNumber: i + 1});
+      });
+      await Promise.all(updates.map(async ({id, orderNumber}) =>
+        db.doc("signups/"+id).set({orderNumber}, {merge: true})
+      ));
+    });
+
