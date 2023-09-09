6528f1d8a870cd1c2a4f20bda729c4bc18f75067
marinao <marina.olivella@loanpro.io>
2023-09-09 13:02:03 -0300

    lint

--- functions/src/mailing.ts
+++ functions/src/mailing.ts
@@ -1,50 +1,51 @@
-import {Change, EventContext} from "firebase-functions";
-import {db} from "./index";
-import {Signup, SignupFirestore, SignupStatus} from "../../src/shared/signup";
-import {firestore} from "firebase-admin";
+import { Change, EventContext } from 'firebase-functions';
+import { db } from './index';
+import { Signup, SignupFirestore, SignupStatus } from '../../src/shared/signup';
+import { firestore } from 'firebase-admin';
 import DocumentSnapshot = firestore.DocumentSnapshot;
 import QueryDocumentSnapshot = firestore.QueryDocumentSnapshot;
 import FieldValue = firestore.FieldValue;
-
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
 
 exports.onUpdateSignup = functions.firestore
-    .document("signups/{signupId}")
-    .onUpdate(async (change: Change<DocumentSnapshot<Signup>>, context: EventContext) => {
-      const signupId = context.params.signupId;
-      console.info(`onUpdate triggered for ${signupId}`);
-      const before = change.before.data();
-      const after = change.after.data();
+  .document('signups/{signupId}')
+  .onUpdate(async (change: Change<DocumentSnapshot<Signup>>, context: EventContext) => {
+    const signupId = context.params.signupId;
+    console.info(`onUpdate triggered for ${signupId}`);
+    const before = change.before.data();
+    const after = change.after.data();
 
-      if (!before || !after) return;
-      if (before.status !== after.status) {
-        const ref = change.after.ref;
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-ignore
-        ref.set({lastModifiedAt: FieldValue.serverTimestamp()}, {merge: true});
-        const mailRef = db.collection("mail");
-        await mailRef.add({
-          to: [after.email],
-          template: {
-            name: after.status,
-            data: {
-              userName: after.nameFirst,
-            },
-          },
-          signupId,
-        });
-      }
-    });
+    if (!before || !after) return;
+    if (before.status !== after.status) {
+      const ref = change.after.ref;
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      ref.set({ lastModifiedAt: FieldValue.serverTimestamp() }, { merge: true });
+      const mailRef = db.collection('mail');
+      await mailRef.add({
+        to: [after.email],
+        template: {
+          name: after.status,
+          data: {
+            userName: after.nameFirst
+          }
+        },
+        signupId
+      });
+    }
+  });
 
 exports.onCreateSignup = functions.firestore
-    .document("signups/{signupId}")
-    .onCreate(async (snapshot: QueryDocumentSnapshot<SignupFirestore>, context: EventContext) => {
-      console.info(`OnCreate triggered for signups/${context.params.signupId}`);
-      await snapshot.ref.set(
-          {status: SignupStatus.WAITLIST,
-            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-            // @ts-ignore
-            lastModifiedAt: FieldValue.serverTimestamp()},
-          {merge: true}
-      );
-    });
+  .document('signups/{signupId}')
+  .onCreate(async (snapshot: QueryDocumentSnapshot<SignupFirestore>, context: EventContext) => {
+    console.info(`OnCreate triggered for signups/${context.params.signupId}`);
+    await snapshot.ref.set(
+      {
+        status: SignupStatus.WAITLIST,
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        lastModifiedAt: FieldValue.serverTimestamp()
+      },
+      { merge: true }
+    );
+  });
