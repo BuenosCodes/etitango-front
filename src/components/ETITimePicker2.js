d48dcfaaabc9df0b656b3da5be08ea16475e2f43
Santiiago8 <santiagopalcios498@gmail.com>
2024-01-24 17:27:25 -0300

    fix input in ETITimePicker2

--- src/components/ETITimePicker2.js
+++ src/components/ETITimePicker2.js
@@ -7,65 +7,84 @@ import InputAdornment from '@mui/material/InputAdornment';
 import { styled } from '@mui/system';
 
 interface ETITimePicker2Props {
-  value: string;
+  value: string | undefined;
   onChange: (value: string) => void;
-  isDisabled: Boolean;
+  isDisabled: boolean;
   showBorders?: boolean;
 }
 
-
 const ETITimePicker2: React.FC<ETITimePicker2Props> = ({ value, onChange, isDisabled, showBorders = true }) => {
-  
-  const StyledTextField = styled(TextField)(({ theme }) => ({
-    '& .MuiOutlinedInput-root': {
-      '& fieldset': {
-        borderColor: showBorders ? (value ? '#E68650' : '#FDE4AA') : 'transparent',
-        borderWidth: showBorders ? 1 : 0,
-      },
-      '&:hover fieldset': {
-        borderColor: showBorders ? '#E68650' : 'transparent',
-      },
-      '&.Mui-focused fieldset': {
-        borderColor: showBorders ? '#E68650' : 'transparent',
-      },
-    },
-  }));
 
-  const handleBlur = (event) => {
-    const time = event.target.value;
-    const isValidTime = /^([01]\d|2[0-3]):([0-5]\d)$/.test(time);
-    if (!isValidTime && time !== '') {
-      console.log('Por favor, ingresa la hora en formato HH:MM');
-      onChange('');
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+  
+    const sanitizedValue = inputValue.replace(/[^0-9:]/g, '');
+  
+    const isValidTime = /^([01]?[0-9]|2[0-3]?):?([0-5]?[0-9]?)?$/.test(sanitizedValue);
+  
+    if (isValidTime || sanitizedValue === '') {
+      if (sanitizedValue.length === 2 && !sanitizedValue.includes(':')) {
+        onChange(sanitizedValue + ':');
+      } else {
+        onChange(sanitizedValue);
+      }
+    }
+  
+    if (sanitizedValue.length >= 2) {
+      const hours = parseInt(sanitizedValue.substring(0, 2));
+      if (hours > 23) {
+        onChange('');
+      }
+    }
+  
+    if (sanitizedValue.length >= 5) {
+      const minutes = parseInt(sanitizedValue.substring(3));
+      if (minutes > 59) {
+        onChange(sanitizedValue.substring(0, 2) + ':');
+      }
     }
   };
-
+  
 
   return (
-   
-    <StyledTextField
-    label=""
-    type="time"
-    value={value}
-    onChange={(event) => onChange(event.target.value)}
-    onBlur={handleBlur}
-    disabled={isDisabled}
-    style={{ width: '102px', height: '48px', borderRadius: '12px'}}
-    InputProps={{
-      startAdornment: (
-        <InputAdornment position="start">
-          <AccessTimeIcon sx={{ color: '#A82548', fontSize: 'large' }} />
-        </InputAdornment>
-      ),
-    }}
-    sx={{
-      '& input[type="time"]::-webkit-calendar-picker-indicator': {
-        display: 'none',
-      },
-    }}
-  />
+    <TextField
+      label=""
+      type="text"
+      value={value}
+      onChange={handleInputChange}
+      onBlur={handleInputChange}
+      disabled={isDisabled}
+      style={{ width: '102px', height: '48px', borderRadius: '12px'}}
+      InputProps={{
+        startAdornment: (
+          <InputAdornment position="start">
+            <AccessTimeIcon sx={{ color: '#A82548', fontSize: 'large' }} />
+          </InputAdornment>
+        ),
+      }}
+      sx={{
+        '& input[type="text"]::-webkit-inner-spin-button, & input[type="text"]::-webkit-outer-spin-button': {
+          '-webkit-appearance': 'none',
+          margin: 0,
+        },
+        '& input[type="text"]': {
+          '-moz-appearance': 'textfield',
+        },
+        '& .MuiOutlinedInput-root': {
+          '& fieldset': {
+            borderColor: showBorders ? (value ? '#E68650' : '#FDE4AA') : 'transparent',
+            borderWidth: showBorders ? 1 : 0,
+          },
+          '&:hover fieldset': {
+            borderColor: showBorders ? '#E68650' : 'transparent',
+          },
+          '&.Mui-focused fieldset': {
+            borderColor: showBorders ? '#E68650' : 'transparent',
+          },
+        },
+      }}
+    />
   );
 };
-
 
 export default ETITimePicker2;
