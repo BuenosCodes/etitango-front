7a6f4cb1bfcd4b7bc21a7ec6a19d0061fd656bdf
thehighestprimenumber <maru.c50@gmail.com>
2024-02-21 10:40:38 -0300

    lazy import

--- src/App.js
+++ src/App.js
@@ -2,35 +2,45 @@ import { Route, Routes } from 'react-router-dom';
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
-import { useState } from 'react';
+import React, { Suspense, useState } from 'react';
 
 import withRoot from './components/withRoot';
 import EtiAppBar from './components/EtiAppBar';
 import AppFooter from './components/AppFooter';
 import { UserContext } from './helpers/UserContext';
 import { NotificationContext } from './helpers/NotificationContext';
-import HistoriaEti from './modules/home/historia-del-ETI/HistoriaEti';
-import ManifiestoETiano from './modules/home/manifiesto-etiano/ManifistoEtiano';
-import ComisionGeneroContact from './modules/home/comision-de-genero/ComisionGeneroContact';
-import ComisionGeneroProtocol from './modules/home/comision-de-genero/ComisionGeneroProtocol';
-import ComisionGeneroWho from './modules/home/comision-de-genero/comisionGeneroWho';
-import Inscripcion from './modules/inscripcion/Inscripcion';
-import SignupList from './modules/inscripcion/SignupList';
-import Receipt from './modules/inscripcion/Receipt';
-import SignInScreen from './modules/signIn/signIn';
-import SuperAdmin from './modules/superAdmin/index';
-import EventsList from './modules/superAdmin/events/EventsList';
-import Profile from './modules/user/profile';
-import UserHome from './modules/user/index';
-import Home from './modules/home/Home';
-import Bank from './modules/user/profile/bank';
 import withUserMenu from './components/withUserMenu';
-import EventForm from './modules/superAdmin/events/EventForm';
-import TemplatesList from './modules/superAdmin/templates';
-import EditTemplate from './modules/superAdmin/templates/EditTemplate';
-import RolesList from './modules/superAdmin/roles/RolesList';
 import { Notification } from './components/notification/Notification';
-import Instructions from './modules/instructions/index';
+import { CircularProgress } from '@mui/material';
+
+const HistoriaEti = React.lazy(() => import('./modules/home/historia-del-ETI/HistoriaEti'));
+const ManifiestoETiano = React.lazy(() =>
+  import('./modules/home/manifiesto-etiano/ManifistoEtiano')
+);
+const ComisionGeneroContact = React.lazy(() =>
+  import('./modules/home/comision-de-genero/ComisionGeneroContact')
+);
+const ComisionGeneroProtocol = React.lazy(() =>
+  import('./modules/home/comision-de-genero/ComisionGeneroProtocol')
+);
+const ComisionGeneroWho = React.lazy(() =>
+  import('./modules/home/comision-de-genero/comisionGeneroWho')
+);
+const Inscripcion = React.lazy(() => import('./modules/inscripcion/Inscripcion'));
+const SignupList = React.lazy(() => import('./modules/inscripcion/SignupList'));
+const Receipt = React.lazy(() => import('./modules/inscripcion/Receipt'));
+const SignInScreen = React.lazy(() => import('./modules/signIn/signIn'));
+const SuperAdmin = React.lazy(() => import('./modules/superAdmin/index'));
+const EventsList = React.lazy(() => import('./modules/superAdmin/events/EventsList'));
+const Profile = React.lazy(() => import('./modules/user/profile'));
+const UserHome = React.lazy(() => import('./modules/user/index'));
+const Home = React.lazy(() => import('./modules/home/Home'));
+const Bank = React.lazy(() => import('./modules/user/profile/bank'));
+const EventForm = React.lazy(() => import('./modules/superAdmin/events/EventForm'));
+const TemplatesList = React.lazy(() => import('./modules/superAdmin/templates'));
+const EditTemplate = React.lazy(() => import('./modules/superAdmin/templates/EditTemplate'));
+const RolesList = React.lazy(() => import('./modules/superAdmin/roles/RolesList'));
+const Instructions = React.lazy(() => import('./modules/instructions/index'));
 
 i18n
   .use(initReactI18next)
@@ -99,41 +109,53 @@ function App() {
         <NotificationContext.Provider value={{ notification, setNotification }}>
           <EtiAppBar />
           <Notification {...notification} />
-          <Routes>
-            <Route path="historia-del-eti" element={<HistoriaEti />} exact />
-            <Route path="manifiesto-etiano" element={<ManifiestoETiano />} exact />
-            <Route path="comision-de-genero-contact" element={<ComisionGeneroContact />} exact />
-            <Route path="comision-de-genero-protocol" element={<ComisionGeneroProtocol />} exact />
-            <Route path="comision-de-genero-who" element={<ComisionGeneroWho />} exact />
-            <Route path={ROUTES.SIGNUP} element={withUserMenu(Inscripcion)()} exact />
-            <Route path={ROUTES.SIGNUPS} element={withUserMenu(SignupList)()} exact />
-            <Route
-              path={ROUTES.ATTENDANCE}
-              element={withUserMenu(SignupList)({ isAttendance: true })}
-              exact
-            />
-            <Route
-              path={`${ROUTES.RECEIPTS}/:etiEventId/:signupId`}
-              element={withUserMenu(Receipt)()}
-            />
-            <Route path={`${ROUTES.RECEIPTS}/:etiEventId`} element={withUserMenu(Receipt)()} />
+          <Suspense
+            fallback={
+              <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+                <CircularProgress />
+              </div>
+            }
+          >
+            <Routes>
+              <Route path="historia-del-eti" element={<HistoriaEti />} exact />
+              <Route path="manifiesto-etiano" element={<ManifiestoETiano />} exact />
+              <Route path="comision-de-genero-contact" element={<ComisionGeneroContact />} exact />
+              <Route
+                path="comision-de-genero-protocol"
+                element={<ComisionGeneroProtocol />}
+                exact
+              />
+              <Route path="comision-de-genero-who" element={<ComisionGeneroWho />} exact />
+              <Route path={ROUTES.SIGNUP} element={withUserMenu(Inscripcion)()} exact />
+              <Route path={ROUTES.SIGNUPS} element={withUserMenu(SignupList)()} exact />
+              <Route
+                path={ROUTES.ATTENDANCE}
+                element={withUserMenu(SignupList)({ isAttendance: true })}
+                exact
+              />
+              <Route
+                path={`${ROUTES.RECEIPTS}/:etiEventId/:signupId`}
+                element={withUserMenu(Receipt)()}
+              />
+              <Route path={`${ROUTES.RECEIPTS}/:etiEventId`} element={withUserMenu(Receipt)()} />
 
-            <Route path={ROUTES.SIGN_IN} element={<SignInScreen />} exact />
-            <Route path={ROUTES.SUPERADMIN} element={<SuperAdmin />} />
-            <Route path={`${ROUTES.SUPERADMIN}${ROUTES.EVENTS}`} element={<EventsList />} />
-            <Route path={`${ROUTES.SUPERADMIN}${ROUTES.EVENTS}/:id`} element={<EventForm />} />
-            <Route path={`${ROUTES.SUPERADMIN}${ROUTES.ROLES}`} element={<RolesList />} />
-            <Route path={ROUTES.USER} element={withUserMenu(UserHome)()} />
-            <Route path={`${ROUTES.BANKS}/:id`} element={<Bank />} />
-            <Route path={ROUTES.PROFILE} element={withUserMenu(Profile)()} />
-            <Route path={ROUTES.HOME} element={<Home />} />
-            <Route path={`${ROUTES.SUPERADMIN}${ROUTES.TEMPLATES}`} element={<TemplatesList />} />
-            <Route
-              path={`${ROUTES.SUPERADMIN}${ROUTES.TEMPLATES}/:id`}
-              element={<EditTemplate />}
-            />
-            <Route path={ROUTES.INSTRUCTIONS} element={<Instructions />} />
-          </Routes>
+              <Route path={ROUTES.SIGN_IN} element={<SignInScreen />} exact />
+              <Route path={ROUTES.SUPERADMIN} element={<SuperAdmin />} />
+              <Route path={`${ROUTES.SUPERADMIN}${ROUTES.EVENTS}`} element={<EventsList />} />
+              <Route path={`${ROUTES.SUPERADMIN}${ROUTES.EVENTS}/:id`} element={<EventForm />} />
+              <Route path={`${ROUTES.SUPERADMIN}${ROUTES.ROLES}`} element={<RolesList />} />
+              <Route path={ROUTES.USER} element={withUserMenu(UserHome)()} />
+              <Route path={`${ROUTES.BANKS}/:id`} element={<Bank />} />
+              <Route path={ROUTES.PROFILE} element={withUserMenu(Profile)()} />
+              <Route path={ROUTES.HOME} element={<Home />} />
+              <Route path={`${ROUTES.SUPERADMIN}${ROUTES.TEMPLATES}`} element={<TemplatesList />} />
+              <Route
+                path={`${ROUTES.SUPERADMIN}${ROUTES.TEMPLATES}/:id`}
+                element={<EditTemplate />}
+              />
+              <Route path={ROUTES.INSTRUCTIONS} element={<Instructions />} />
+            </Routes>
+          </Suspense>
           <AppFooter />
         </NotificationContext.Provider>
       </UserContext.Provider>
