1b54f669f2f54b3c3ac05c9aaa83d1757d0b1a38
Misael-Cortes <misael.cortes39@gmail.com>
2024-03-26 10:32:28 -0300

    fix language datePicker

--- src/components/withRoot.js
+++ src/components/withRoot.js
@@ -4,6 +4,14 @@ import theme from '../theme';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { GlobalStateProvider } from 'helpers/UserPanelContext';
+import moment from 'moment';
+import 'moment/locale/es';
+moment.lang('es', {
+  months:
+    'Enero_Febrero_Marzo_Abril_Mayo_Junio_Julio_Agosto_Septiembre_Octubre_Noviembre_Diciembre'.split(
+      '_'
+    )
+});
 
 export default function withRoot(Component) {
   function WithRoot(props) {
