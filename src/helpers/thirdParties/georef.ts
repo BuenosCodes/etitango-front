b09e1dbd789ca374c9fe9f3dfc0af4695570f3ab
Niv Sardi <xaiki@evilgiggle.com>
2025-02-17 20:03:00 -0300

    implement argentina selector
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/helpers/thirdParties/georef.ts
+++ src/helpers/thirdParties/georef.ts
@@ -1,7 +1,7 @@
 import { toTitleCase } from '../formatting';
 
 // https://datosgobar.github.io/georef-ar-api/
-interface ArgentinaLocalidades {
+interface ArgentinaLocalidadesDatosGobAr {
   cantidad: number;
   localidades: {
     id: string;
@@ -9,7 +9,7 @@ interface ArgentinaLocalidades {
   }[];
 }
 
-interface ArgentinaProvincias {
+interface ArgentinaProvinciasDatosGobAr {
   cantidad: number;
   provincias: {
     nombre: string;
@@ -17,16 +17,64 @@ interface ArgentinaProvincias {
   }[];
 }
 
-export const getProvinces = async () => {
+// https://github.com/juanifioren/argentina-states-cities/tree/master
+export interface ArgentinaProvinciaLocal {
+  code: string;
+  name: string;
+}
+
+const ProvinciasMap : { [id: string] : string;  } = {
+  "Capital Federal": "CAB",
+  "Buenos Aires": "BSA",
+  "Catamarca": "CAT",
+  "Córdoba": "COR",
+  "Corrientes": "CRR",
+  "Chaco": "CHA",
+  "Chubut": "CHU",
+  "Entre Ríos": "ENT",
+  "Formosa": "FOR",
+  "Jujuy": "JUJ",
+  "La Pampa": "PAM",
+  "La Rioja": "RIO",
+  "Mendoza": "MEN",
+  "Misiones": "MIS",
+  "Neuquén": "NEU",
+  "Río Negro": "RNE",
+  "Salta": "SAL",
+  "San Juan": "SJU",
+  "San Luis": "SLU",
+  "Santa Cruz": "SCR",
+  "Santa Fe": "SFE",
+  "Santiago del Estero": "SDE",
+  "Tierra del Fuego": "TDF",
+  "Tucumán": "TUC"
+};
+
+export const getProvincesDatosGobAr = async () => {
   const response = await fetch('https://apis.datos.gob.ar/georef/api/provincias?campos=nombre');
-  const { provincias } = (await response.json()) as ArgentinaProvincias;
+  const { provincias } = (await response.json()) as ArgentinaProvinciasDatosGobAr;
   return (provincias || []).map((province) => province.nombre).sort();
 };
-export const getCities = async (province: string) => {
+export const getCitiesDatosGobAr = async (province: string) => {
   const encodedProvince = encodeURIComponent(province);
   const response = await fetch(
     `https://apis.datos.gob.ar/georef/api/localidades?provincia=${encodedProvince}&max=2000&campos=nombre`
   );
-  const data = (await response.json()) as ArgentinaLocalidades;
+  const data = (await response.json()) as ArgentinaLocalidadesDatosGobAr;
   return (data?.localidades || []).map((localidad) => toTitleCase(localidad.nombre)).sort();
 };
+
+export const getProvinces = async () => {
+  const response = await fetch('/data/argentina_states.json');
+  const provincias  = (await response.json()) as ArgentinaProvinciaLocal[];
+  return (provincias || []).map((province) => province.name).sort();
+}
+
+export const getCities = async (province: string) => {
+  const code = ProvinciasMap[province];
+  const response = await fetch(
+    `/data/argentina_localities_${code}.json`
+  );
+  const localidades = (await response.json()) as string[];
+  return localidades
+}
--- src/modules/inscripcion/SignupForm.tsx
+++ src/modules/inscripcion/SignupForm.tsx
@@ -54,6 +54,7 @@ export function SignupForm() {
       city,
       wantsLodging
     } = values;
+
     let data = {
       dateArrival,
       dateDeparture,
